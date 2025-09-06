3060ec1ebab2fb90e974dce8f2cf17fd02d07d6d
NoahSheppard <sheppardn@ofgsstudents.com>
2025-09-06 19:00:19 +1000

    shift/uppercase support

--- kernel/kb.c
+++ kernel/kb.c
@@ -1,5 +1,7 @@
 #include "system.h"
 
+int shift_held = 0;
+
 //unsigned char kbdus[128] = 
 //{
 //    0 /*no scancode: 0*/, '\e', /*esc*/ '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '\b', /* backspace*/
@@ -53,6 +55,47 @@ unsigned char kbdus[128] =
     0,	/* All other keys are undefined */
 };	
 
+unsigned char skbdus[128] =
+{
+    0,  27, '!', '@', '#', '$', '%', '^', '&', '*',	/* 9 */
+  '(', ')', '_', '+', '\b',	/* Backspace */
+  '\t',			/* Tab */
+  'Q', 'W', 'E', 'R',	/* 19 */
+  'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '\n',	/* Enter key */
+    0,			/* 29   - Control */
+  'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':',	/* 39 */
+ '\"', '~',   0,		/* Left shift */
+ '|', 'Z', 'X', 'C', 'V', 'B', 'N',			/* 49 */
+  'M', '<', '>', '?',   0,				/* Right shift */
+  '*',
+    0,	/* Alt */
+  ' ',	/* Space bar */
+    0,	/* Caps lock */
+    0,	/* 59 - F1 key ... > */
+    0,   0,   0,   0,   0,   0,   0,   0,
+    0,	/* < ... F10 */
+    0,	/* 69 - Num lock*/
+    0,	/* Scroll Lock */
+    0,	/* Home key */
+    0,	/* Up Arrow */
+    0,	/* Page Up */
+  '-',
+    0,	/* Left Arrow */
+    0,
+    0,	/* Right Arrow */
+  '+',
+    0,	/* 79 - End key*/
+    0,	/* Down Arrow */
+    0,	/* Page Down */
+    0,	/* Insert Key */
+    0,	/* Delete Key */
+    0,   0,   0,
+    0,	/* F11 Key */
+    0,	/* F12 Key */
+    0,	/* All other keys are undefined */
+};	
+
+
 void debug_keyboard_handler(struct regs *r)
 {
     unsigned char scancode;
@@ -98,19 +141,38 @@ void debug_keyboard_handler(struct regs *r)
 void keyboard_handler(struct regs *r)
 {
     unsigned char scancode;
+    
     scancode = importb(0x60);
     if (scancode & 0x80)
     {
-        // when key is released
+        switch (scancode)
+        {
+            // 42 54
+            case 0xAA: 
+            case 0xB6: 
+            {
+                shift_held = 0;
+                return;
+            }
+            default: { return; }
+        }
     }
-    else if (scancode == 0x1C)
+    if (scancode == 0x1C)
     {
         basicterm_return_handler();
         return;
     }
-    else
+    else if (scancode == 0x2A || scancode == 0x36)
     {
-        basicterm_key_handler(kbdus[scancode]);
+        shift_held = 1;
+        return;
+    }
+    else if (kbdus[scancode] != 0)
+    {
+        if (shift_held == 1)
+            basicterm_key_handler(skbdus[scancode]);
+        else if (shift_held == 0)
+            basicterm_key_handler(kbdus[scancode]);
     }
 }
 
